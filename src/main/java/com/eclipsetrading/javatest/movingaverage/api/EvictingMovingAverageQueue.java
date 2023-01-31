4a66a48bed4b22acedb16b70b3de0dc44209213e
dasabz <dasabhishek057@gmail.com>
2023-01-31 17:54:31 +0800

    MovingAverage

--- src/main/java/com/eclipsetrading/javatest/movingaverage/api/EvictingMovingAverageQueue.java
+++ src/main/java/com/eclipsetrading/javatest/movingaverage/api/EvictingMovingAverageQueue.java
@@ -1,6 +1,6 @@
 package com.eclipsetrading.javatest.movingaverage.api;
 
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -8,13 +8,19 @@ public class EvictingMovingAverageQueue<T> {
     private int size = 0;
     private final int capacity;
     private double currSum;
-    private final LinkedBlockingDeque<Double> queue;
+    private final ArrayBlockingQueue<Double> queue;
     private final Lock lock = new ReentrantLock();
     public EvictingMovingAverageQueue(int capacity){
         this.capacity = capacity;
-        this.queue    = new LinkedBlockingDeque<>(capacity);
+        this.queue    = new ArrayBlockingQueue<>(capacity);
     }
 
+    /**
+     * As the values are inserted at the tail of the arrayBlockingQueue always it is O(1)
+     * When the size reaches the capacity, then the head is removed which is again O(1)
+     * The currSum is eagerly maintained so that the average can be quickly computed when requested
+     * @param value
+     */
     public void offer(double value){
         lock.lock();
         double valueRemoved = 0;
